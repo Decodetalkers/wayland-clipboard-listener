e5b72be3e32e47ecb2be09ae043a7771ae380104
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-06-19 14:03:40 +0800

    chore: finishe wl-copy

--- bin/marine_copy.rs
+++ bin/marine_copy.rs
@@ -1,4 +1,10 @@
+use libc::{STDIN_FILENO, STDOUT_FILENO};
+use nix::{
+    fcntl::OFlag,
+    unistd::{close, dup2, fork, ForkResult},
+};
 use wayland_clipboard_listener::{WlClipboardCopyStream, WlClipboardListenerError};
+
 fn main() -> Result<(), WlClipboardListenerError> {
     let args = std::env::args();
     if args.len() != 2 {
@@ -7,6 +13,17 @@ fn main() -> Result<(), WlClipboardListenerError> {
     }
     let context: &str = &args.last().unwrap();
     let mut stream = WlClipboardCopyStream::init()?;
-    stream.copy_to_clipboard(context.as_bytes().to_vec(), false)?;
+
+    if let Ok(ForkResult::Child) = unsafe { fork() } {
+        if let Ok(dev_null) =
+            nix::fcntl::open("/dev/null", OFlag::O_RDWR, nix::sys::stat::Mode::empty())
+        {
+            let _ = dup2(dev_null, STDIN_FILENO);
+            let _ = dup2(dev_null, STDOUT_FILENO);
+            let _ = close(dev_null);
+            stream.copy_to_clipboard(context.as_bytes().to_vec(), false)?;
+        }
+    }
+
     Ok(())
 }
